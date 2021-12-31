e6f3de3567dd638d91214e39b69597a22b53fe18
zakiya adam <zakiyaadan12@gmail.com>
2021-12-31 18:26:51 +0300

    added button submit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-/* ARRAYS OF THE days*/
+function day(){
+  let day = document.getElementById("day-of-birth").value;
+  let month = document.getElementById("month-of-birth").value;
+  let year = document.getElementById("year-of-birth").value;
+  let gender = document.getElementById("gender").value;
+  console.log(day);
+  console.log(month);
+  console.log(year);
+  console.log(gender);
+  
+}
+
