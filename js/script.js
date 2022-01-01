67a8e0714c407c36b925bdf2f5f6d45d8f380a11
zakiya adam <zakiyaadan12@gmail.com>
2022-01-01 13:39:21 +0300

    added var of weekday

--- js/script.js
+++ js/script.js
@@ -8,15 +8,37 @@ function day(){
   console.log(year);
   console.log(gender);
 
-}
-/* arrays of the female akan name*/
-let femaleAkanNames =[
+  /* arrays of the female akan name*/
+  let femaleAkanNames =[
   "akosua", "adwoa", "abenaa", "akua", "yaa", "afua", "ama",  
-]
-/* arrays of the male akan name */
-let maleAkanNames =[
+  ]
+  /* arrays of the male akan name */
+  let maleAkanNames =[
   "kwasi", "kwadko", "kwabena", "kwaku", "yaw", "kofi", "kwame",
-]
-let DaysOfTheWeek =[
-  "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", 
-] 
+
+var dob = month + " " + day + ", " + year;
+var then = new Date(dob);
+var theday = then.getDay()+1;
+var date=then.getDate();
+var year=then.getYear();
+if (year<100) year="19" + then.getYear();
+var weekday = new Array(6);
+weekday[1]="Sunday";
+weekday[2]="Monday";
+weekday[3]="Tuesday";
+weekday[4]="Wednesday";
+weekday[5]="Thursday";
+weekday[6]="Friday";
+weekday[7]="Saturday";
+if (day != date) alert("Sorry!  That appears to be an invalid date!");
+else {
+dayborn = weekday[theday];
+dob = dayborn + ", " + month + " " + date + ", " + year + ".";
+alert("You were born on " + dob);
+   }
+
+
+
+
+
+}
