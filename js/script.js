e7d350920e3785ecb2c6e9847b9853a9ee0aae75
zakiya adam <zakiyaadan12@gmail.com>
2022-01-03 21:36:09 +0300

    made some changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-function day(){
-  let day = document.getElementById("day-of-birth").value;
-  let month = document.getElementById("month-of-birth").value;
-  let year = document.getElementById("year-of-birth").value;
-  let gender = document.getElementById("gender").value;
-  console.log(day);
-  console.log(month);
-  console.log(year);
-  console.log(gender);
+function date(){
+  let day = parseInt(document.getElementById("day-of-birth").value);
+  let month = parseInt(document.getElementById("month-of-birth").value);
+  let year = parseInt(document.getElementById("year-of-birth").value);
+  let gender;
+
+  
+  
 
   /* arrays of the female akan name*/
   let femaleAkanNames =[
@@ -15,30 +14,6 @@ function day(){
   /* arrays of the male akan name */
   let maleAkanNames =[
   "kwasi", "kwadko", "kwabena", "kwaku", "yaw", "kofi", "kwame",
-
-var dob = month + " " + day + ", " + year;
-var then = new Date(dob);
-var theday = then.getDay()+1;
-var date=then.getDate();
-var year=then.getYear();
-if (year<100) year="19" + then.getYear();
-var weekday = new Array(6);
-weekday[1]="Sunday";
-weekday[2]="Monday";
-weekday[3]="Tuesday";
-weekday[4]="Wednesday";
-weekday[5]="Thursday";
-weekday[6]="Friday";
-weekday[7]="Saturday";
-if (day != date) alert("Sorry!  That appears to be an invalid date!");
-else {
-dayborn = weekday[theday];
-dob = dayborn + ", " + month + " " + date + ", " + year + ".";
-alert("You were born on " + dob);
-   }
-
-
-
-
-
+  ]
+  
 }
